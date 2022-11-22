8d055c71dd13117e44ae0ea8fa81a2cc6ff8c4a3
peraltalaura <75114015+peraltalaura@users.noreply.github.com>
2022-11-22 14:17:41 +0100

    update test

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -3,8 +3,4 @@
 include "docker-compose/www/conf.php";
 include "docker-compose/www/includes/mysql.php";
 
-$creds = mysqli_query($conx,"SELECT * FROM users WHERE username='". $username ."'");
-
-$creds = mysqli_fetch_array($creds);
-
 ?>
