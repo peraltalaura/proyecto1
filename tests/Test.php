a56b841f2ba3faac6dcf1dde52dc8e54b9d9438f
peraltalaura <75114015+peraltalaura@users.noreply.github.com>
2022-11-23 09:25:28 +0100

    update test

--- tests/Test.php
+++ tests/Test.php
@@ -3,9 +3,13 @@
     use PHPUnit\Framework\TestCase;
     use App\includes\funtzioak;
 
-    $string_mal="<script>alert('hola')</script>";
-    $string_sanitizada=escape($string_mal);
+    class CalculatorTest extends TestCase {
 
-    echo $string_sanitizada;
+        $string_mal="<script>alert('hola')</script>";
+        $string_sanitizada=escape($string_mal);
+
+        echo $string_sanitizada;
+
+    }
 
 ?>
