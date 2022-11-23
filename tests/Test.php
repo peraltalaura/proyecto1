8dc745728bbca68e809042f554aeaae54d251bfe
peraltalaura <75114015+peraltalaura@users.noreply.github.com>
2022-11-23 09:28:26 +0100

    fix

--- tests/Test.php
+++ tests/Test.php
@@ -3,7 +3,7 @@
     use PHPUnit\Framework\TestCase;
     use App\includes\funtzioak;
 
-    class CalculatorTest extends TestCase {
+    class Test extends TestCase {
 
         function escape($html) {
             $htmlsanitizado=htmlspecialchars($html, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
