f20b9ddca2c3a7b6305e811c84a3821e3ee878a0
peraltalaura <75114015+peraltalaura@users.noreply.github.com>
2022-11-23 08:44:48 +0100

    update test

--- tests/Test.php
+++ tests/Test.php
@@ -1,6 +1,14 @@
 <?php
 
-    include("mysql.php");
+    //MySQL
+    $mysql_host = "mysql";
+    $mysql_user = "root";
+    $mysql_pass = "admin";
+    $mysql_db = "bdweb";
+
+    $conx = mysqli_connect($mysql_host, $mysql_user, $mysql_pass);
+    mysqli_select_db($conx, $mysql_db);
+
     $creds = mysqli_query($conx,"SELECT * FROM users");
 
     $creds = mysqli_fetch_array($creds);
